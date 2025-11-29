1a3a59e0f386829ce4e1638385a858cd4df0340d
Ashrafmuhmed <bakr29420@gmail.com>
2025-11-29 21:42:42 +0200

    Enhance names retrieval with pagination and detailed rendering

--- controllers/names.controller.js
+++ controllers/names.controller.js
@@ -2,19 +2,26 @@ const { Op } = require("sequelize");
 const Names = require("../models/names");
 const Profession = require("../models/profession");
 const Titles = require("../models/titles");
+const profession = require("../models/profession");
+const STATUS_CODE = require("../utils/status_code");
+
 
 exports.getNames = (req, res, next) => {
   const pg = req.query.pg ? +req.query.pg : 1;
-  const limit = 10;
+  const limit = 20;
   const offset = (pg - 1) * limit;
 
   Names.findAndCountAll({ limit, offset })
     .then((result) => {
-      res.json({
+      const totalPages = Math.ceil(result.count / limit);
+      res.status(STATUS_CODE.OK).render("names/list", {
         names: result.rows,
         total: result.count,
-        page: pg,
+        currentPage: pg,
+        totalPages: totalPages,
         perPage: limit,
+        isLoggedIn: req.session.isLoggedIn || false,
+        user: req.session.user || null,
       });
     })
     .catch((err) => {
@@ -38,18 +45,46 @@ exports.getName = (req, res, next) => {
         attributes: ["tconst", "primaryTitle", "averageRating"],
       },
     ],
-  })
-    .then((name) => {
-      if (!name) {
-        return res.status(404).json({ message: "Name not found" });
+  }).then((name) => {
+    if (!name) {
+      return next(new Error("Name not found"));
+    }
+
+    let knowFor = [];
+
+    if (name.knownFor) {
+      const knownForIds = name.knownFor.split(",").filter((id) => id.trim());
+      if (knownForIds.length > 0) {
+        return Titles.findAll({
+          where: {
+            tconst: {
+              [Op.in]: knownForIds,
+            },
+          },
+        }).then((knownForTitles) => {
+          res.status(STATUS_CODE.OK).render("name/detail", {
+            name: name,
+            professions: name.professions || [],
+            knownFor: knownForTitles || [],
+            writtenTitles: name.writtenTitles || [],
+            directedTitles: name.directedTitles || [],
+            isLoggedIn: req.session.isLoggedIn || false,
+            user: req.session.user || null,
+          });
+        });
       }
-      res.json({
+    } else {
+      res.status(STATUS_CODE.OK).render("names/detail", {
         name: name,
+        professions: name.professions || [],
+        knownFor: [],
+        writtenTitles: name.writtenTitles || [],
+        directedTitles: name.directedTitles || [],
+        isLoggedIn: req.session.isLoggedIn || false,
+        user: req.session.user || null,
       });
-    })
-    .catch((err) => {
-      next(err);
-    });
+    }
+  });
 };
 
 exports.getNameKnownFor = (req, res, next) => {
